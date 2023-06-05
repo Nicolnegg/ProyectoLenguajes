c5dee8fed62b7be901e13f5f01e68a5b2e38fe98
AngelSilva2002 <apenarredonda@unal.edu.co>
2023-06-04 19:40:48 -0500

    Comit angel

--- src/Linea.java
+++ src/Linea.java
@@ -1,14 +1,28 @@
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.util.Arrays;
+
 public class Linea {
+
+    private static InterfazProyecto interfazProyecto;
+
     public static void main(String[] args) throws Exception {
-        String linea= "a+=2";
-        // create a CharStream that reads from standard input / file
-        // create a lexer that feeds off of input CharStream
+
+        String linea = interfazProyecto.getLineaActual();
+
+        analizarlinea(linea);
+    }
+
+
+    public static void analizarlinea(String linea) {
+
         PythonLexer lexer;
+
 
         lexer = new PythonLexer(CharStreams.fromString(linea));
 
@@ -23,6 +37,5 @@ public class Linea {
         VisitorsComentarios visitor = new VisitorsComentarios();
         visitor.visit(tree);
         System.out.println(); // print a \n after translation
-
     }
 }
