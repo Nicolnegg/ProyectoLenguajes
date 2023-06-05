12366c46468ac8fac6a123600f5e15ae582de7e7
Nicolnegg <princesnegg@gmail.com>
2023-06-04 21:32:38 -0500

    variables

--- src/VisitorsComentarios.java
+++ src/VisitorsComentarios.java
@@ -31,35 +31,67 @@ public class VisitorsComentarios  extends PythonParserBaseVisitor<Void> {
                 String[] valordiv= valorText.split("=");
                 //valordiv[0] queda eelmoperador, valordiv[1] queda la asignacion
                 char es_lista=valordiv[1].charAt(0);
-                if (es_lista=='['){
+                String variable = ctx.testlist_star_expr().getText();
+                char es_tupla=variable.charAt(0);
+                if (es_tupla=='('){
+                    variable = variable.substring(1, valordiv[1].length() - 1);
 
                 }
-                Object valor = engine.eval(valordiv[1]);
-                String variable = ctx.testlist_star_expr().getText();
-                if(variables_global.contains(variable)){
-                    int posicion = variables_global.indexOf(variable);
-                    if(valordiv[0].equals("")){
-                        valores_global.set(posicion,valor.toString());
+                else{
+                    if (es_lista=='['){
+                        valordiv[1] = valordiv[1].substring(1, valordiv[1].length() - 1);
+                        String[] elementos = valordiv[1].split(",");
+                        List<String> lista = new ArrayList<>();
+                        for (String elemento : elementos) {
+                            lista.add(elemento); // Agrega el elemento a la lista
+                        }
+                        if(variables_global.contains(variable)){
+
+                            int posicion = variables_global.indexOf(variable);
+                            if(valordiv[0].equals("")){
+
+                                valores_global.set(posicion,lista);
+                            }else if(valordiv[0].equals("+")){
+                                Object obj = valores_global.get(posicion);
+                                if (obj instanceof List) {
+                                    List<Object> valor = (List<Object>) obj;
+                                    valor.addAll(lista);
+                                }
+                            }else{
+                                throw new RuntimeException("Error: No se pueden realizar esta operacion en una lista");
+                            }
+                        }
+                        else if(!valordiv[0].equals("")){
+                            throw new RuntimeException("Error: No se pueden realizar operaciones con variables inexistentes");
+                        }
+                        else{
+                            valores_global.add(lista);
+                            variables_global.add(variable);
+                        }
                     }else{
-                        Object masigual = engine.eval(  valores_global.get(posicion)+ valordiv[0] +valor.toString());
-                        valores_global.set(posicion,masigual.toString());
+                        Object valor = engine.eval(valordiv[1]);
+                        if(variables_global.contains(variable)){
+                            int posicion = variables_global.indexOf(variable);
+                            if(valordiv[0].equals("")){
+                                valores_global.set(posicion,valor.toString());
+                            }else{
+                                Object masigual = engine.eval(  valores_global.get(posicion)+ valordiv[0] +valor.toString());
+                                valores_global.set(posicion,masigual.toString());
+                            }
+                        }
+                        else if(!valordiv[0].equals("")){
+                            throw new RuntimeException("Error: No se pueden realizar operaciones con variables inexistentes");
+                        }
+                        else{
+                            valores_global.add(valor.toString());
+                            variables_global.add(variable);
+                        }
                     }
                 }
-                else if(!valordiv[0].equals("")){
-                    throw new RuntimeException("Error: No se pueden realizar operaciones con variables inexistentes");
-                }
-                else{
-                    valores_global.add(valor.toString());
-                    variables_global.add(variable);
-                }
+
                 Object obj = valores_global.get(0);
-                System.out.println(obj);
-                if (obj instanceof ArrayList) {
-                    ArrayList<Object> lista = (ArrayList<Object>) obj;
-                    System.out.println(lista.get(0).toString());
-                } else if (obj instanceof Object[]) {
-                    Object[] array = (Object[]) obj;
-                    ArrayList<Object> lista = new ArrayList<>(Arrays.asList(array));
+                if (obj instanceof List) {
+                    List<Object> lista = (List<Object>) obj;
                     System.out.println(lista.get(0).toString());
                 } else if (obj instanceof String) {
                     String str = (String) obj;
